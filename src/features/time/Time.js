064c4c6bde890824a0ccd10fc633d725240ad1a8
Keanu Williams <keanucwilliams@gmail.com>
2021-08-02 23:33:14 -1000

    Finished creating Time component

---
+++ src/features/time/Time.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  updateTime,
+  selectTime
+} from './timeSlice';
+
+export const Time = () => {
+  const time = useSelector(selectTime);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setInterval(() => {
+      dispatch(updateTime())
+    }, 1000);
+  }, [dispatch]);
+
+  return <span>{time}</span>;
+}
