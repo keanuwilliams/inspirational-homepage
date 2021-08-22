b565a9cf9f8be382299d4e1785a792dfd49bbbcf
Keanu Williams <keanucwilliams@gmail.com>
2021-08-22 11:53:20 -1000

    Created background files and deleted default counter files

---
+++ src/features/background/backgroundSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const backgroundSlice = createSlice({
+  name: 'background',
+  initialState: {},
+  reducers: {}
+});
+
+export default backgroundSlice.reducer;
