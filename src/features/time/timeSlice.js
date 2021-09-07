f8be2b9b45782c180dbfa1988884f29278f204e9
Keanu Williams <keanucwilliams@gmail.com>
2021-09-06 23:53:22 -1000

    #43 - Added ability to display seconds for time

--- src/features/time/timeSlice.js
+++ src/features/time/timeSlice.js
@@ -1,11 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 // Creates a time string depending on the one boolean parameter: militaryTime
-const getTimeString = (militaryTime) => {
+const getTimeString = (militaryTime = false, secondsPreference = false) => {
   // Grab the hours and minutes to create the time string
   const date = new Date();
   let hours = date.getHours();
   let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
 
   // Create the time string
   let timeString = "";
@@ -23,44 +24,65 @@ const getTimeString = (militaryTime) => {
       AMPM = "AM";
     }
   }
-  // Add a leading zero if hours and minutes are one digit
+  // Add a leading zero if hours, minutes, or seconds are one digit
   hours = hours < 10 ? `0${hours}` : `${hours}`;
   minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
-  timeString = `${hours}:${minutes}`;
+  seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+  // Check if user prefers the seconds added
+  timeString = secondsPreference ? `${hours}:${minutes}:${seconds}` : `${hours}:${minutes}`;
   if (!militaryTime) {
     timeString += AMPM;
   }
   return timeString;
 }
 
-const getTimePreference = () => {
-  const json = localStorage.getItem('timePreference');
-  if (json !== null) {
-    return JSON.parse(json);
-  } else {
-    return false;
+const getInitialState = () => {
+  const jsonMilitary = localStorage.getItem('timeMilitary');
+  const jsonSeconds = localStorage.getItem('timeSeconds');
+  let initialState = {
+    militaryTime: false,
+    secondsPreference: false,
+    value: ''
+  };
+
+  // Check if military time exists locally, and if it does get the data
+  if (jsonMilitary !== null) {
+    initialState.militaryTime = JSON.parse(jsonMilitary);
   }
+
+  // Check if seconds preference exists locally, and if it does get the data
+  if (jsonSeconds !== null) {
+    initialState.secondsPreference = JSON.parse(jsonSeconds);
+  }
+
+  initialState.value = getTimeString(initialState.militaryTime, initialState.secondsPreference);
+
+  return initialState;
 }
 
 export const timeSlice = createSlice({
   name: 'time',
-  initialState: {
-    value: getTimeString(false),
-    militaryTime: getTimePreference(),
-  },
+  initialState: getInitialState(),
   reducers: {
     toggleTime: (state) => {
       state.militaryTime = !state.militaryTime;
       const json = JSON.stringify(state.militaryTime);
-      localStorage.setItem('timePreference', json);
+      localStorage.setItem('timeMilitary', json);
+    },
+    toggleSeconds: (state) => {
+      state.secondsPreference = !state.secondsPreference;
+      const json = JSON.stringify(state.secondsPreference);
+      localStorage.setItem('timeSeconds', json);
     },
     updateTime: (state) => {
-      state.value = getTimeString(state.militaryTime);
+      state.value = getTimeString(state.militaryTime, state.secondsPreference);
     }
   }
 });
 
 export const selectMilitaryTime = (state) => state.time.militaryTime;
 export const selectTime = (state) => state.time.value;
-export const { toggleTime, updateTime } = timeSlice.actions;
+export const selectSecondsPreference = (state) => state.time.secondsPreference;
+export const { toggleTime, updateTime, toggleSeconds } = timeSlice.actions;
 export default timeSlice.reducer;
