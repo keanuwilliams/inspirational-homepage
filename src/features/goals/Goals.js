a68e3b28e84f9a76025df57b53f3c9c971f85011
Keanu Williams <keanucwilliams@icloud.com>
2022-01-09 14:18:39 -1000

    Removed the "all" buttons for goals
    Didn't really like how it looked like. Plus, the use case for it was not really common.

--- src/features/goals/Goals.js
+++ src/features/goals/Goals.js
@@ -5,14 +5,9 @@ import {
   completeGoal,
   toggleEdit,
   updateGoal,
-  comepleteAllGoals,
-  removeAllGoals
 } from "./goalsSlice";
 import { useSelector, useDispatch } from "react-redux";
 import Goal from './Goal';
-import Button from '../../components/Button/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faBan } from '@fortawesome/free-solid-svg-icons';
 import './Goals.css';
 
 /**
@@ -22,15 +17,8 @@ export default function Goals() {
   const goals = useSelector(selectGoals);
   const dispatch = useDispatch();
 
-  const checkmarkIcon = <FontAwesomeIcon icon={faCheck} />;
-  const banIcon = <FontAwesomeIcon icon={faBan} />;
-
   return (
     <>
-      <div className="goal-all-btns">
-        {goals.length !== 0 && <Button success contents={checkmarkIcon} onClick={() => dispatch(comepleteAllGoals())} />}
-        {goals.length !== 0 && <Button danger contents={banIcon} onClick={() => dispatch(removeAllGoals())}/>}
-      </div>
       <section className="goals">
         {Object.values(goals).map((goal) => (
             <Goal 
